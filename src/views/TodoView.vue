8cedb91d28640d908cdc85c0c2c4ed06d4c3340d
Rizki36 <cahrizki.rf@gmail.com>
2022-04-13 20:04:14 +0700

    initial commit

---
+++ src/views/TodoView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import TodoItem from "@/components/TodoItem.vue";
+</script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      todos: [{}, {}],
+      newTodo: "",
+      editedTodo: null,
+      visibility: "all",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <h3>Todo</h3>
+      <button>Create Reminder</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  background-color: aquamarine;
+  height: 1000px;
+}
+</style>
